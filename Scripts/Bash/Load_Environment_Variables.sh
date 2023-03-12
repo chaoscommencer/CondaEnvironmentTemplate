cc97526d57d6f739fc0d1bbb50f7f6829bd2e300
David Chamberlain <chaoscommencer@users.noreply.github.com>
2023-03-12 13:14:44 -0400

    Support Python 3.11.0 and Argcomplete
    - Additionally, automatically add Conda completions
    
    Signed-off-by: David Chamberlain <chaoscommencer@users.noreply.github.com>

--- Scripts/Bash/Load_Environment_Variables.sh
+++ Scripts/Bash/Load_Environment_Variables.sh
@@ -3,4 +3,13 @@
 # Backup current environment variables to support restoration on Conda
 # environment deactivation
 
+# Backup current PYTHONPATH
+export PYTHONPATH_BAK="${PYTHONPATH}"
+
 # Define Conda-environment-specific variables
+
+# Define PYTHONPATH to enable Python module detection
+export PYTHONPATH="${CONDA_PREFIX}/python_modules"
+
+# Enable tab command completions for Conda
+eval "$(register-python-argcomplete conda)"
--- Scripts/Bash/Unload_Environment_Variables.sh
+++ Scripts/Bash/Unload_Environment_Variables.sh
@@ -2,4 +2,10 @@
 
 # Restore environment variables to original definitions
 
+# Restore PYTHONPATH
+export PYTHONPATH="${PYTHONPATH_BAK}"
+
 # Undefine intermediate *_BAK environment variables
+
+# Undefine intermediate PYTHONPATH_BAK
+unset PYTHONPATH_BAK
